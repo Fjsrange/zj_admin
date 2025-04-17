2c6eeae8e3b5c5957120e012806b672678e719b3
Orange <106434581+Fjsrange@users.noreply.github.com>
2025-04-17 10:10:13 +0800

    new file

---
+++ src/Layout/content/index.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts"></script>
+
+<template>
+  <router-view></router-view>
+</template>
+
+<style scoped lang="scss">
+* {
+  height: 100%;
+  margin: 10px;
+  background: #fff;
+  border-radius: 5px;
+}
+</style>
---
+++ src/Layout/index.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import Menu from "./menu/index.vue";
+import Header from "./header/index.vue";
+import Content from "./content/index.vue";
+</script>
+
+<template>
+  <div class="l-layout">
+    <div>
+      <Menu></Menu>
+    </div>
+    <div class="l-layout__right">
+      <Header></Header>
+      <Content></Content>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+@import "@/bem.scss";
+
+@include b(layout) {
+  @include bfc();
+  display: flex;
+
+  @include e(right) {
+    display: flex;
+    flex-direction: column;
+    flex: 1;
+  }
+}
+</style>
